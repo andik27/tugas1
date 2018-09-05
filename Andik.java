23aece36438d7ee7e1dabfc1e994a9ed0b6b126b
andik27 <42985928+andik27@users.noreply.github.com>
2018-09-05 07:35:24 +0700

    Add files via upload

---
+++ Andik.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package andik;
+
+/**
+ *
+ * @author asus
+ */
+public class Andik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          int x=5;
+        for (int a=1;a<=5;a++){
+            for(int b=1;b<=a;b++){
+                System.out.print("*");
+            }
+            System.out.println();
+    }
+    
+}}
